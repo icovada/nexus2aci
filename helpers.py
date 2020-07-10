69ad0042e55b654be44cead641aca58bd7dd98d6
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2020-07-10 10:52:11 +0200

    Working

--- helpers.py
+++ helpers.py
@@ -13,15 +13,16 @@ def allowed_vlan_to_list(vlanlist):
 def parse_vlan_l2(conf):
     vlans = conf.find_objects(r"^vlan \d")
     l2dict = {}
-    for vlan in vlans.ioscfg:
-        vlan_id = int(vlan[0].split()[-1])
-        if "name" in str(vlan):
-            namelist = [x for x in vlan if " name " in x]
+    for vlan in vlans:
+        vlancfg = vlan.ioscfg
+        vlan_id = int(vlancfg[0].split()[-1])
+        if "name" in str(vlancfg):
+            namelist = [x for x in vlancfg if " name " in x]
             assert len(namelist) == 1
             vlan_name_raw = namelist[0]
-            vlan_name = vlan_name_raw.replace(" name ").strip()
-            l2dict[vlan_id] = {}
-        else: 
+            vlan_name = vlan_name_raw.replace(" name ", "").strip()
             l2dict[vlan_id] = {"name": vlan_name}
+        else: 
+            l2dict[vlan_id] = {}
 
     return l2dict
