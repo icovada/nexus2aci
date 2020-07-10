75a6b9cca80091fd9cfa62e6f597dc7919387939
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2020-07-10 14:07:46 +0200

    Add tests for parse_svi

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@ import unittest
 
 from helpers import allowed_vlan_to_list
 from helpers import parse_vlan_l2
+from helpers import parse_svi
 
 
 class TestAllowedVlanToList(unittest.TestCase):
@@ -53,7 +54,6 @@ class TestParseVlanL2(unittest.TestCase):
 
         assert parse_vlan_l2(test_data) == output
 
-
     def test_parse_vlan_l2_name_in_name(self):
         config = ["vlan 3",
                   "  name name",
@@ -66,6 +66,134 @@ class TestParseVlanL2(unittest.TestCase):
                   4: {"name": "anothername"}}
 
         assert parse_vlan_l2(test_data) == output
+
+
+class TestParseSVI(unittest.TestCase):
+    from ciscoconfparse import CiscoConfParse
+
+    def test_parse_svi(self):
+        config = ["vlan 3",
+                  "  name FRONT-END",
+                  "vlan 4",
+                  "  name EX_CED_SDM",
+                  "",
+                  "interface Vlan3",
+                  "  vrf member VRF-VINTAGE",
+                  "  no ip redirects",
+                  "  ip address 172.26.54.41/26",
+                  "  hsrp version 2",
+                  "  hsrp 3 ",
+                  "    authentication md5 key-chain hsrp-vintage",
+                  "    priority 150",
+                  "    timers  5  15",
+                  "    ip 172.26.54.62 ",
+                  "  description SRG-FRONT-END_NEW_SYS-SAT-CMS-W2K",
+                  "  no shutdown",
+                  "",
+                  "interface Vlan4",
+                  "  vrf member VRF-VINTAGE",
+                  "  no ip redirects",
+                  "  ip address 172.22.178.110/22",
+                  "  hsrp version 2",
+                  "  hsrp 4 ",
+                  "    authentication md5 key-chain hsrp-vintage",
+                  "    priority 150",
+                  "    timers  5  15",
+                  "    ip 172.22.176.9 ",
+                  "  description ***VLAN_CED_CAVASSI***",
+                  "  no shutdown"]
+
+        test_data = self.CiscoConfParse(config)
+
+        output = {3: {"name": "FRONT-END",
+                      "vrf": "VRF-VINTAGE",
+                      "l3": {"ip_address": "172.26.54.62",
+                             "netmask": "255.255.255.192"},
+                      "description": "SRG-FRONT-END_NEW_SYS-SAT-CMS-W2K",
+                      "shutdown": False},
+                  4: {"name": "EX_CED_SDM",
+                      "vrf": "VRF-VINTAGE",
+                      "l3": {"ip_address": "172.22.176.9",
+                             "netmask": "255.255.252.0"},
+                      "description": "***VLAN_CED_CAVASSI***",
+                      "shutdown": False}}
+
+        assert parse_svi(test_data) == output
+
+    def test_parse_svi_no_hsrp(self):
+        config = ["vlan 3",
+                  "  name FRONT-END",
+                  "",
+                  "interface Vlan3",
+                  "  vrf member VRF-VINTAGE",
+                  "  no ip redirects",
+                  "  ip address 172.26.54.41/26",
+                  "  description SRG-FRONT-END_NEW_SYS-SAT-CMS-W2K",
+                  "  no shutdown"]
+
+        test_data = self.CiscoConfParse(config)
+
+        output = {3: {"name": "FRONT-END",
+                      "vrf": "VRF-VINTAGE",
+                      "l3": {"ip_address": "172.26.54.41",
+                             "netmask": "255.255.255.192"},
+                      "description": "SRG-FRONT-END_NEW_SYS-SAT-CMS-W2K",
+                      "shutdown": False}}
+
+    def test_parse_svi_shutdown(self):
+        config = ["vlan 3",
+                  "  name FRONT-END",
+                  "",
+                  "interface Vlan3",
+                  "  vrf member VRF-VINTAGE",
+                  "  no ip redirects",
+                  "  ip address 172.26.54.41/26",
+                  "  description SRG-FRONT-END_NEW_SYS-SAT-CMS-W2K",
+                  "  shutdown"]
+
+        test_data = self.CiscoConfParse(config)
+
+        output = {3: {"name": "FRONT-END",
+                      "vrf": "VRF-VINTAGE",
+                      "l3": {"ip_address": "172.26.54.41",
+                             "netmask": "255.255.255.192"},
+                      "description": "SRG-FRONT-END_NEW_SYS-SAT-CMS-W2K",
+                      "shutdown": True}}
+        assert parse_svi(test_data) == output
+
+    def test_parse_svi_no_vrf(self):
+        config = ["vlan 3",
+                  "  name FRONT-END",
+                  "",
+                  "interface Vlan3",
+                  "  no ip redirects",
+                  "  ip address 172.26.54.41/26",
+                  "  description SRG-FRONT-END_NEW_SYS-SAT-CMS-W2K",
+                  "  no shutdown"]
+
+        test_data = self.CiscoConfParse(config)
+
+        output = {3: {"name": "FRONT-END",
+                      "vrf": "default",
+                      "l3": {"ip_address": "172.26.54.41",
+                             "netmask": "255.255.255.192"},
+                      "description": "SRG-FRONT-END_NEW_SYS-SAT-CMS-W2K",
+                      "shutdown": True}}
+        assert parse_svi(test_data) == output
+
+    def test_parse_svi_no_ip(self):
+        config = ["interface Vlan1",
+                  "  no ip redirects",
+                  "  no ipv6 redirects"]
+
+        test_data = self.CiscoConfParse(config)
+
+        output = {1: {"name": "FRONT-END",
+                      "vrf": "default",
+                      "l3": {"ip_address": "172.26.54.41",
+                             "netmask": "255.255.255.192"},
+                      "shutdown": True}}
+        assert parse_svi(test_data) == output
 
 
 if __name__ == '__main__':
