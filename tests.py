743b8ab1d04a6f73d602e4e46887c19f75986f96
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2020-07-10 09:35:39 +0200

    Format files with autopep8

--- tests.py
+++ tests.py
@@ -2,19 +2,20 @@ import unittest
 
 from helpers import allowed_vlan_to_list
 
-class Test_allowed_vlan_to_list(unittest.TestCase):
-    
+
+class TestAllowedVlanToList(unittest.TestCase):
+
     def test_only_commas(self):
         test_data = "1,2,3,4,5"
-        output = (1,2,3,4,5)
+        output = (1, 2, 3, 4, 5)
         assert allowed_vlan_to_list(test_data) == output
 
     def test_only_ranges(self):
         test_data = "1-5"
-        output = (1,2,3,4,5)
+        output = (1, 2, 3, 4, 5)
         assert allowed_vlan_to_list(test_data) == output
 
     def test_comma_and_range(self):
         test_data = "1,2,3-5"
-        output = (1,2,3,4,5)
+        output = (1, 2, 3, 4, 5)
         assert allowed_vlan_to_list(test_data) == output
