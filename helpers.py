8161cc3c5193a86ace52db89f18c0c1e8d7eb649
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2020-07-10 10:30:44 +0200

    Add parse_vlan_l2

--- helpers.py
+++ helpers.py
@@ -10,5 +10,18 @@ def allowed_vlan_to_list(vlanlist):
             outlist = outlist + (int(vlan), )
     return outlist
 
-if __name__ == "__main__":
-    pass
+def parse_vlan_l2(conf):
+    vlans = conf.find_objects(r"^vlan \d")
+    l2dict = {}
+    for vlan in vlans.ioscfg:
+        vlan_id = int(vlan[0].split()[-1])
+        if "name" in str(vlan):
+            namelist = [x for x in vlan if " name " in x]
+            assert len(namelist) == 1
+            vlan_name_raw = namelist[0]
+            vlan_name = vlan_name_raw.replace(" name ").strip()
+            l2dict[vlan_id] = {}
+        else: 
+            l2dict[vlan_id] = {"name": vlan_name}
+
+    return l2dict
