3bd0d62d27c2371e3758ccae1bbad0327fd2cf6c
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2020-07-10 09:52:11 +0200

    Add boilerplate

--- tests.py
+++ tests.py
@@ -19,3 +19,6 @@ class TestAllowedVlanToList(unittest.TestCase):
         test_data = "1,2,3-5"
         output = (1, 2, 3, 4, 5)
         assert allowed_vlan_to_list(test_data) == output
+
+if __name__ == '__main__':
+    unittest.main()
