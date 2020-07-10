f837e29ab803b1b5c6f242017f6ba287e4fc274a
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2020-07-10 17:17:30 +0200

    More minimal implementation

--- helpers.py
+++ helpers.py
@@ -14,15 +14,10 @@ def parse_vlan_l2(conf):
     vlans = conf.find_objects(r"^vlan \d")
     l2dict = {1: {}}
     for vlan in vlans:
-        vlancfg = vlan.ioscfg
-        vlan_id = int(vlancfg[0].split()[-1])
-        if "name" in str(vlancfg):
-            namelist = [x for x in vlancfg if " name " in x]
-            assert len(namelist) == 1
-            vlan_name_raw = namelist[0]
-            vlan_name = vlan_name_raw.replace(" name ", "").strip()
+        vlan_id = int(vlan.text.split()[-1])
+        l2dict[vlan_id] = {}
+        for vlan in vlan.re_search_children("name"):
+            vlan_name = vlan.text.replace(" name ", "").strip()
             l2dict[vlan_id] = {"name": vlan_name}
-        else: 
-            l2dict[vlan_id] = {}
 
     return l2dict
