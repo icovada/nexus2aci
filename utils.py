5498e54cf1931bd6dbca228c3b55b1785db5d916
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2020-07-23 19:13:14 +0200

    Add find_key

--- utils.py
+++ utils.py
@@ -3,4 +3,13 @@ def traverse_dict(dict, path):
     if len(path) == 0:
         return dict
     else:
-        return traverse(dict[path
+        return traverse_dict[path])
+
+def find_key(d, value):
+    for k,v in d.items():
+        if isinstance(v, dict):
+            p = find_key(v, value)
+            if p:
+                return [k] + p
+        elif v == value:
+            return [k]
