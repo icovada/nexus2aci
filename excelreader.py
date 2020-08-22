57de7c371e958881dc89ca81229cb0e13c3c1a5e
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2020-08-22 10:56:52 +0200

    bd details in bd list inside tenant, not epg

--- excelreader.py
+++ excelreader.py
@@ -43,32 +43,45 @@ for tenant in clean_tenant_list:
     app_list = thistenant.ANP.unique().tolist()
     clean_app_list = [x for x in app_list if str(x) != 'nan']
     allapp = []
+    all_tenant_bd = []
 
     for app in clean_app_list:
+        app_bd_list = []
         thisapp = thistenant.loc[thistenant['ANP'] == app]
         epg_list = thisapp.EPG.unique().tolist()
         clean_epg_list = [x for x in epg_list if str(x) != 'nan']
-        allepg = []
+        all_app_epg = []
 
         for epg in clean_epg_list:
             thisepg = thisapp.loc[thisapp['EPG'] == epg]
             bd_list = thisepg.BD.unique().tolist()
             clean_bd_list = [x for x in bd_list if str(x) != 'nan']
-
             assert len(clean_bd_list) == 1
+
+            for bd in clean_bd_list:
+                thisbd = thisepg.loc[thisepg['BD'] == bd]
+                thisrow = thisbd.iloc[0]
+                vrf = thisrow['VRF-NEW']
+                bddict = deepcopy(default_bd)
+                bddict.update({'name': bd,
+                               'vrf': vrf})
+                all_tenant_bd.append(bddict)
+
+            epgdict = deepcopy(default_epg)
+            epgdict.update({'name': epg,
+                            'bd': bddict['name']})
+            all_app_epg.append(epgdict)
 
             epgdict = deepcopy(default_epg)
             epgdict.update({'name': epg,
                             'bd': clean_bd_list[0]})
-            allepg.append(epgdict)
+
+        
 
         appdict = deepcopy(default_app)
         appdict.update({'name': app,
-                        'epg': allepg})
+                        'epg': all_app_epg})
         allapp.append(appdict)
-
-    bd_list = thistenant['BD'].unique().tolist()
-    clean_bd_list = [x for x in bd_list if str(x) != 'nan']
 
     vrf_list = thistenant['VRF-NEW'].unique().tolist()
     clean_vrf_list = [x for x in vrf_list if str(x) != 'nan']
@@ -76,7 +89,7 @@ for tenant in clean_tenant_list:
     tenantdict = deepcopy(default_tenant)
     tenantdict.update({'name': tenant,
                        'app': allapp,
-                       'bd': clean_bd_list,
+                       'bd': all_tenant_bd,
                        'vrf': clean_vrf_list})
     alltenant.append(tenantdict)
 
