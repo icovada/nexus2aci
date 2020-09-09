b8c1455949fedf7d1929be8f2c1dc49dc1cd8f3b
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2020-09-09 19:38:47 +0200

    Accept kwargs for bundle properties

--- helpers/bundle.py
+++ helpers/bundle.py
@@ -1,9 +1,9 @@
 from cobra.model.infra import AccBndlGrp
 from cobra.mit.request import ConfigRequest
 
-def create_bundle_interface(name, moDir, vpc=False):
+def create_bundle_interface(name, moDir, vpc=False, *args, **kwargs):
     parent = moDir.lookupByDn('uni/infra/funcprof')
-    bundle = AccBndlGrp(parent, name)
+    bundle = AccBndlGrp(parent, name, **kwargs)
     if vpc is True:
         bundle.lagT = "node"
 
