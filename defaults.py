c8d3ad1804743f2e30234bb1cd96f04ca5d8de53
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2020-09-10 12:44:51 +0200

    Typo

--- defaults.py
+++ defaults.py
@@ -25,9 +25,9 @@ UNSAFE_CHARACTER_REPLACE = {"&": "_e_",}
 
 INTERFACE_SELECTOR_ACCESS = f"access_{name}_IntSel"
 INTERFACE_SELECTOR_BUNDLE = f"{bundlemode}_GDC_{name}_IntSel"
-INTERFACE_SELECTOR_PROFILE = f"Leaf-{'-'.join([str(x) for x in leaves])_IntProf}"
-LEAF_PROFILE = f"Leaf-{'-'.join([str(x) for x in leaves])_LeafProf}"
-LEAF_SELECTOR = f"Leaf-{'-'.join([str(x) for x in leaves])_SwSel}"
+INTERFACE_SELECTOR_PROFILE = f"Leaf-{'-'.join([str(x) for x in leaves])}_IntProf"
+LEAF_PROFILE = f"Leaf-{'-'.join([str(x) for x in leaves])}_LeafProf"
+LEAF_SELECTOR = f"Leaf-{'-'.join([str(x) for x in leaves])}_SwSel"
 
 POLICY_GROUP_BUNDLE = f"{bundlemode}_GDC_{name}_PolGrp"
 POLICY_GROUP_ACCESS = f"access_SRV_1G-auto_PolGrp"
