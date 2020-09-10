a1c90fa70e4c02168b5a43a82ed792f3c7cf5f93
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2020-09-10 10:41:06 +0200

    port_channel is lacp_modes

--- policymappings.py
+++ policymappings.py
@@ -1,3 +1,3 @@
-port_channel = {"on": "Static_mode_on",
-                "active": "LACP_Active_toNonNexus",
-                "passive": "LACP_Active_toNonNexus"}
+lacp_modes = {"on": "Static_mode_on",
+              "active": "LACP_Active_toNonNexus",
+              "passive": "LACP_Active_toNonNexus"}
