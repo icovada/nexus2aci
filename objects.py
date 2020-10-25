3092624a789d5efd36fc5492a2f7a9811f34446b
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2020-10-25 12:39:14 +0100

    Add more checks in objects

--- objects.py
+++ objects.py
@@ -9,18 +9,41 @@ class Interface():
     allowed_vlan:Optional[List[int]] = []
 
     def __init__(self, name:str):
-        print(type(self))
         self.name = name
 
     def allowed_vlan_add(self, newvlans:List[int]):
         self.allowed_vlan = self.allowed_vlan + newvlans
+    
+    def is_useless(self) -> bool:
+        if len(self.__dict__) == 1:
+            return True
+        else:
+            return False
 
 class PortChannel(Interface):
-    vpc:int = 0
-    members = []
+    vpc:Optional[int]
+    po_id:int
+    members:List[Interface]
+
+    def __init__(self, name:str):
+        print(type(self))
+        if type(self) == PortChannel:
+            # Do not run if we are being subclassed
+            self.po_id = int(name[12:])
+        super().__init__(name)
+    
+    def is_useful(self) -> bool:
+        # If has no members, it's useless
+        if hasattr(self, "members"):
+            if len(self.members) > 0:
+                return True
+            else:
+                return False
+        else:
+            return False
 
 class Vpc(PortChannel):
-    def __init__(self, name:str, vpcid:int):
+    def __init__(self, vpcid:int):
         self.vpcid = vpcid
-        print(type(self))
+        name = "vpc" + str(vpcid)
         super().__init__(name)
