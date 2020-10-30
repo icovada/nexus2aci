a9b8eab6291a61d938153ffb941f3ff6b950d749
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2020-10-30 16:12:09 +0100

    Adapt for objects

--- helpers/int.py
+++ helpers/int.py
@@ -1,26 +1,27 @@
+from objects import PortChannel, Vpc, Interface
 from cobra.model.infra import AccBndlGrp, PortBlk, RsLacpPol
 from cobra.mit.request import ConfigRequest
 import policymappings
 
 def create_bundle_interface_polgrp(interface, parent, *args, **kwargs):
-    if "port-channel" in interface['name']:
+    if type(interface) == PortChannel:
         lagT = "link"
-    elif "vpc" in interface['name']:
+    elif type(interface) == Vpc:
         lagT = "node"
     else:
         raise KeyError("This shouldn't happen")
 
-    bundle = AccBndlGrp(parent, interface['newname'], lagT=lagT)
+    bundle = AccBndlGrp(parent, interface.newname, lagT=lagT)
     
     try:
-        assert interface['protocol'] in policymappings.lacp_modes
-        lacp = interface['protocol']
+        assert interface.protocol in policymappings.lacp_modes
+        lacp = interface.protocol
     except KeyError:
         # protocol not in interface
         lacp = "on"
     except AssertionError:
         # protocol value not in lacp_modes
-        raise AssertionError("Unknown protocol value " + str(interface['protocol']) + ", set it in policymappings.lacp_modes")
+        raise AssertionError("Unknown protocol value " + str(interface.protocol) + ", set it in policymappings.lacp_modes")
     finally:
         lacp = policymappings.lacp_modes.get(lacp)
 
@@ -30,7 +31,7 @@ def create_bundle_interface_polgrp(interface, parent, *args, **kwargs):
 
 
 def create_port_block(interface, portselector):
-    path = interface['newname'].split("/")
+    path = interface.newname.split("/")
 
     if "-" in path[2]:
         fromPort, toPort = path[2].split("-")
@@ -41,7 +42,7 @@ def create_port_block(interface, portselector):
 
     block = PortBlk(portselector, 
                     f"ethernet_{path[2]}",
-                    descr=interface.get("description", ""), 
+                    descr=interface.description, 
                     fromCard=int(path[1]),
                     toCard=int(path[1]),
                     fromPort=int(fromPort),
