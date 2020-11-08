1cd4a6832e653994e35a19acbf4ee5e071addf23
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2020-11-08 12:47:55 +0100

    Output new CSV format

--- parse_conf.py
+++ parse_conf.py
@@ -1,6 +1,6 @@
 import csv
 import pickle
-from objects import PortChannel, Vpc
+from objects import Interface, PortChannel, Vpc
 from libs import parse_nexus_pair_l2
 from filelist import entiredc
 from helpers.generate_excel import generate_excel
@@ -22,17 +22,41 @@ for interface in parseddc:
 allintdata = []
 for i in parseddc:
     # We don't care about port channels that are part of a vpc
-    if type(i) == PortChannel and i.ismember:
-        continue
-    thisint = {"newname": "",
-               "name": str(i),
-               "description": i.description if hasattr(i, "description") else ""}
-    allintdata.append(thisint)
+    if type(i) == Interface:
+        if i.ismember:
+            continue
+        else:
+            thisint = {"newname": "",
+                       "vpc": "",
+                       "portchannel": "",
+                       "name": str(i),
+                       "description": i.description if hasattr(i, "description") else ""}
+            allintdata.append(thisint)
+
+    elif type(i) == PortChannel and not i.ismember:
+        for member in i.members:
+            thisint = {"newname": "",
+                       "vpc": "",
+                       "portchannel": str(i),
+                       "name": str(member),
+                       "description": member.description if hasattr(member, "description") else ""}
+            allintdata.append(thisint)
+
+    elif type(i) == Vpc:
+        for pomember in i.members:
+            for member in pomember.members:
+                thisint = {"newname": "",
+                           "vpc": str(i),
+                           "portchannel": str(pomember),
+                           "name": str(member),
+                           "description": member.description if hasattr(member, "description") else ""}
+                allintdata.append(thisint)
+    
 
 
 with open("intnames.csv", "w") as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=[
-        "name", "description", "newname"])
+        "name", "vpc", "portchannel", "description", "newname"])
     writer.writeheader()
     writer.writerows(allintdata)
 
