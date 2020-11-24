de5cad077d5d4ee60dbaea7df0fef6ccb5a92258
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2020-11-24 15:28:19 +0100

    Updated file list

--- filelist.py
+++ filelist.py
@@ -1,9 +1,12 @@
 # parse configs for access and distribution switches
 # you should theoretically add all switches in the DC
 
-entiredc = {'agg': {"conf": ['config/AGGSNANGDC-1.nxos', 'config/AGGSNANGDC-2.nxos']},
-            'aggp': {"conf": ['config/AGGSNANGDCP-1.nxos', 'config/AGGSNANGDCP-2.nxos']},
-            'core': {"conf": ['config/CORESNANGDC-1.nxos', 'config/CORESNANGDC-2.nxos']},
+entiredc = {'agg': {"conf": ['config/AGGSNANGDC-1.nxos', 'config/AGGSNANGDC-2.nxos'],
+                    "intstatus": ['config/AGGSNANGDC-1.intstatus', 'config/AGGSNANGDC-2.intstatus']},
+            'aggp': {"conf": ['config/AGGSNANGDCP-1.nxos', 'config/AGGSNANGDCP-2.nxos'],
+                     "intstatus": ['config/AGGSNANGDCP-1.intstatus', 'config/AGGSNANGDCP-2.intstatus']},
+            'core': {"conf": ['config/CORESNANGDC-1.nxos', 'config/CORESNANGDC-2.nxos'],
+                     "intstatus": ['config/CORESNANGDC-1.intstatus', 'config/CORESNANGDC-2.intstatus']},
             'H1-2': {"conf": ['config/SWISNA-NGDC5K-H1.nxos', 'config/SWISNA-NGDC5K-H2.nxos'],
                      "intstatus": ['config/SWISNA-NGDC5K-H1.intstatus', 'config/SWISNA-NGDC5K-H2.intstatus']},
             'H3-4': {"conf": ['config/SWISNA-NGDC5K-H3.nxos', 'config/SWISNA-NGDC5K-H4.nxos'],
